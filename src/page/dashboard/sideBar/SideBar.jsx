7c21eb886ca472c52bd929f417a4c8601478e82d
Genzi135 <phu13052002@gmail.com>
2024-01-12 11:16:23 +0700

    fix dropdown setting and add todo for sidebar

--- src/page/dashboard/sideBar/SideBar.jsx
+++ src/page/dashboard/sideBar/SideBar.jsx
@@ -1,12 +1,32 @@
 import React from "react";
-import { BsChatDotsFill, BsChatDots, BsJournalBookmark, BsJournalBookmarkFill, BsGear, BsGearFill, BsChevronRight, BsPerson, BsDatabase, BsTools, BsInfoCircle, BsGlobe } from "react-icons/bs";
+import { BsChatDotsFill, BsChatDots, BsJournalBookmark, BsJournalBookmarkFill, BsGear, BsGearFill, BsCheckSquare, BsCheckSquareFill } from "react-icons/bs";
+import DropdownSetting from "./component/dropdownSetting";
 
 export default function SideBar() {
     const [selectedItem, setSelectedItem] = React.useState("chat");
+    const [showDropdown, setShowDropdown] = React.useState(false);
+    const dropdownRef = React.useRef(null);
+
+    const handleClickOutside = (event) => {
+        if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+            setShowDropdown(false);
+        }
+    }
 
     const handleItemClick = (item) => {
-        setSelectedItem(item);
+        if (item === "settings") {
+            setShowDropdown(!showDropdown);
+        } else {
+            setSelectedItem(item);
+        }
     };
+
+    React.useEffect(() => {
+        document.addEventListener("click", handleClickOutside);
+        return () => {
+            document.removeEventListener("click", handleClickOutside)
+        }
+    }, [])
 
     return (
         <div style={{ width: 80, backgroundColor: "blue", display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: "center" }}>
@@ -34,44 +54,84 @@ export default function SideBar() {
                     <div
                         onClick={() => handleItemClick("chat")}
                         style={{
-                            backgroundColor: selectedItem === "chat" ? "darkBlue" : "blue",
                             width: "100%",
                             height: 80,
-                            display: "flex",
-                            flexDirection: "column",
-                            justifyContent: "center",
-                            alignItems: "center",
                         }}
+                        className="hover:bg-blue-600"
                     >
-                        {selectedItem === "chat" ? (
-                            <BsChatDotsFill size={30} color={"white"} />
-                        ) : (
-                            <BsChatDots size={30} color={"white"} />
-                        )}
+                        <div
+                            style={{
+                                backgroundColor: selectedItem === "chat" ? "darkBlue" : "",
+                                width: "100%",
+                                height: "100%",
+                                display: "flex",
+                                flexDirection: "column",
+                                justifyContent: "center",
+                                alignItems: "center",
+                            }}>
+                            {selectedItem === "chat" ? (
+                                <BsChatDotsFill size={30} color={"white"} />
+                            ) : (
+                                <BsChatDots size={30} color={"white"} />
+                            )}
+                        </div>
                     </div>
                     <div
                         onClick={() => handleItemClick("contact")}
                         style={{
-                            backgroundColor: selectedItem === "contact" ? "darkBlue" : "blue",
                             width: "100%",
                             height: 80,
-                            display: "flex",
-                            flexDirection: "column",
-                            justifyContent: "center",
-                            alignItems: "center",
+
                         }}
+                        className="hover:bg-blue-600"
                     >
-                        {selectedItem === "contact" ? (
-                            <BsJournalBookmarkFill size={30} color={"white"} />
-                        ) : (
-                            <BsJournalBookmark size={30} color={"white"} />
-                        )}
+                        <div
+                            style={{
+                                backgroundColor: selectedItem === "contact" ? "darkBlue" : "",
+                                width: "100%",
+                                height: "100%",
+                                display: "flex",
+                                flexDirection: "column",
+                                justifyContent: "center",
+                                alignItems: "center",
+                            }}>
+                            {selectedItem === "contact" ? (
+                                <BsJournalBookmarkFill size={30} color={"white"} />
+                            ) : (
+                                <BsJournalBookmark size={30} color={"white"} />
+                            )}
+                        </div>
+                    </div>
+                    <div
+                        onClick={() => handleItemClick("todo")}
+                        style={{
+                            width: "100%",
+                            height: 80,
+
+                        }}
+                        className="hover:bg-blue-600"
+                    >
+                        <div
+                            style={{
+                                backgroundColor: selectedItem === "todo" ? "darkBlue" : "",
+                                width: "100%",
+                                height: "100%",
+                                display: "flex",
+                                flexDirection: "column",
+                                justifyContent: "center",
+                                alignItems: "center",
+                            }}>
+                            {selectedItem === "todo" ? (
+                                <BsCheckSquareFill size={30} color={"white"} />
+                            ) : (
+                                <BsCheckSquare size={30} color={"white"} />
+                            )}
+                        </div>
                     </div>
                 </div>
                 <div
-                    className="dropdown dropdown-top hover:bg-blue-700"
+                    ref={dropdownRef}
                     style={{
-                        backgroundColor: selectedItem === "settings" ? "darkBlue" : "blue",
                         width: "100%",
                         height: 80,
                         display: "flex",
@@ -80,47 +140,17 @@ export default function SideBar() {
                         alignItems: "center",
                         alignSelf: 'flex-end'
                     }}
+                    onClick={() => handleItemClick("settings")}
+                    className="hover:bg-blue-600"
                 >
-                    <div tabIndex={0} role="button">
-                        {selectedItem === "settings" ? (
-                            <BsGearFill size={30} color={"white"} />
-                        ) : (
-                            <BsGear size={30} color={"white"} />
-                        )}
-                        <ul tabIndex={0} className="dropdown-content z-[1] menu bg-white drop-shadow-lg border" style={{ width: 250, marginBottom: 2, borderRadius: 5, gap: 10, padding: 10, color: 'black', display: "block" }}>
-                            <li>
-                                <a className="dropdown-item"><BsPerson size={20} /> Account information</a>
-                            </li>
-                            <li>
-                                <a className="dropdown-item"><BsGear size={20} /> Settings</a>
-                            </li>
-                            <div style={{ width: "100%", borderBottomWidth: 0.5 }}></div>
-                            <li>
-                                <a className="dropdown-item" style={{ display: 'flex', justifyContent: 'space-between' }}><div style={{ display: 'flex', gap: 10 }}><BsDatabase size={20} /> Data</div> <BsChevronRight size={15} color={"black"} /></a>
-                            </li>
-                            <li>
-                                <a className="dropdown-item" style={{ display: 'flex', justifyContent: 'space-between' }}><div style={{ display: 'flex', gap: 10 }}><BsTools size={20} /> Tools</div> <BsChevronRight size={15} color={"black"} /></a>
-                            </li>
-                            <li>
-                                <a className="dropdown-item" style={{ display: 'flex', justifyContent: 'space-between' }}>
-                                    <div style={{ display: 'flex', gap: 10 }} tabIndex={2} role="button" >
-                                        <BsGlobe size={20} />
-                                        Language
-                                    </div>
-                                    <BsChevronRight size={15} color={"black"} />
-                                </a>
-                            </li>
-                            <li>
-                                <a className="dropdown-item" style={{ display: 'flex', justifyContent: 'space-between' }}><div style={{ display: 'flex', gap: 10 }}><BsInfoCircle size={20} /> Tools</div> <BsChevronRight size={15} color={"black"} /></a>
-                            </li>
-                            <div style={{ width: "100%", borderBottomWidth: 0.5 }}></div>
-                            <li>
-                                <a className="dropdown-item" style={{ color: 'red' }} >Logout</a>
-                            </li>
-                        </ul>
-                    </div>
-
+                    {selectedItem === "settings" ? (
+                        <BsGearFill size={30} color={"white"} />
+                    ) : (
+                        <BsGear size={30} color={"white"} />
+                    )}
+                    {showDropdown && <DropdownSetting />}
                 </div>
+
             </div>
         </div>
     );
---
+++ src/page/dashboard/sideBar/component/DropdownSetting.jsx
@@ -0,0 +1,115 @@
+import { BsGear, BsChevronRight, BsPerson, BsDatabase, BsTools, BsInfoCircle, BsGlobe } from "react-icons/bs";
+
+export default function DropdownSetting() {
+    return (
+        <>
+            <div className=""
+                style={{ marginBottom: 330, marginLeft: 270 }}>
+                <div tabIndex={0} role="button">
+                    <ul tabIndex={0} className="dropdown-content z-[1] menu bg-white drop-shadow-lg border" style={{ width: 250, marginBottom: 2, borderRadius: 5, gap: 10, color: 'black', display: "block" }}>
+                        <li>
+                            <a ><BsPerson size={20} /> Account information</a>
+                        </li>
+                        <li>
+                            <a ><BsGear size={20} /> Settings</a>
+                        </li>
+                        <div style={{ width: "100%", borderBottomWidth: 0.5 }}></div>
+                        <li>
+                            <div className="dropdown dropdown-hover dropdown-right">
+                                <div style={{ display: 'flex', justifyContent: 'space-between' }} tabIndex={0} role="button">
+                                    <div style={{ display: 'flex', gap: 10, marginRight: 123 }}>
+                                        <BsDatabase size={20} />
+                                        Data
+                                    </div>
+                                    <div>
+                                        <BsChevronRight size={15} color={"black"} />
+                                        <div tabIndex={0} className="dropdown-content z-[1] shadow-md" style={{ backgroundColor: 'white', padding: 10, gap: 10 }}>
+                                            <ul>
+                                                <li>
+                                                    <div style={{ width: 140, height: 40 }}>Manage files</div>
+                                                </li>
+
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="dropdown dropdown-hover dropdown-right">
+                                <div style={{ display: 'flex', justifyContent: 'space-between' }} tabIndex={0} role="button">
+                                    <div style={{ display: 'flex', gap: 10, marginRight: 120 }}>
+                                        <BsTools size={20} />
+                                        Tools
+                                    </div>
+                                    <div>
+                                        <BsChevronRight size={15} color={"black"} />
+                                        <div tabIndex={0} className="dropdown-content z-[1] shadow-md" style={{ backgroundColor: 'white', padding: 10, gap: 10 }}>
+                                            <ul>
+                                                <li>
+                                                    <div style={{ width: 140, height: 40 }}>Send log files</div>
+                                                </li>
+
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="dropdown dropdown-hover dropdown-right">
+                                <div style={{ display: 'flex', justifyContent: 'space-around' }}
+                                    className="" tabIndex={0} role="button">
+                                    <div style={{ display: 'flex', gap: 10, marginRight: 90 }}>
+                                        <BsGlobe size={20} />
+                                        Language
+                                    </div>
+                                    <div>
+                                        <BsChevronRight size={15} color={"black"} />
+                                        <div tabIndex={0} className="dropdown-content z-[1] shadow-md" style={{ backgroundColor: 'white', padding: 10, gap: 10 }}>
+                                            <ul>
+                                                <li>
+                                                    <div style={{ width: 140, height: 40 }}>Tiếng Việt</div>
+                                                </li>
+                                                <li>
+                                                    <div style={{ width: 140, height: 40 }}>English</div>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="dropdown dropdown-hover dropdown-right">
+                                <div style={{ display: 'flex', justifyContent: 'space-between' }} tabIndex={0} role="button">
+                                    <div style={{ display: 'flex', gap: 10, width: 100, marginRight: 81 }}>
+                                        <BsInfoCircle size={20} />
+                                        About Zalo
+                                    </div>
+                                    <div>
+                                        <BsChevronRight size={15} color={"black"} />
+                                        <div tabIndex={0} className="dropdown-content z-[1] shadow-md" style={{ backgroundColor: 'white', padding: 10, gap: 10 }}>
+                                            <ul>
+                                                <li>
+                                                    <div style={{ width: 140, height: 40 }}>Version</div>
+                                                </li>
+                                                <li>
+                                                    <div style={{ width: 140, height: 40 }}>Hepper Center</div>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <div style={{ width: "100%", borderBottomWidth: 0.5 }}></div>
+                        <li>
+                            <a style={{ color: 'red' }} >Logout</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    )
+}
